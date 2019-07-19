6fdf49383b5efd8452b92a9d5dd2021b72389766
郑智勇 <zhengzhiyong@insightworks.cn>
2019-07-19 23:17:19 +0800

    完善并发请求,返回结果完毕清除定时器

--- ConcurrentRequest.js
+++ ConcurrentRequest.js
@@ -6,6 +6,7 @@
 /* eslint-disable import/prefer-default-export */
 export function ConcurrentRequest(dispatch, requests, priCallback, that) {
   const responses = [];
+  let requestsTimer = {};
   for (let i = 0; i < requests.length; i++) {
     responses.push({});
   }
@@ -13,7 +14,7 @@ export function ConcurrentRequest(dispatch, requests, priCallback, that) {
   for (let i = 0; i < requests.length; i++) {
     if (requests[i].type && requests[i].payload) {
       const data = requests[i];
-      setTimeout(function (data) {
+      requestsTimer[i]=setTimeout(function (data) {
         dispatch({
           type: data.type,
           payload: data.payload,
@@ -21,6 +22,9 @@ export function ConcurrentRequest(dispatch, requests, priCallback, that) {
           responses[i]=response.data;
           successRequestCount++;
           if (requests.length == successRequestCount) {
+            for (var each in requestsTimer) {
+              clearInterval(requestsTimer[each]);
+            }
              priCallback(responses, that);
           }
         })
